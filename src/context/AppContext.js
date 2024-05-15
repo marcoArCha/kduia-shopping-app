b76516f28786f8e9b3ee07d86cd01fa20afce8a6
Marco Arias <marcoarccha@hotmail.com>
2024-05-15 10:14:12 -0600

    Components added

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -56,7 +56,8 @@ export const AppReducer = (state, action) => {
             return state;
     }
 };
-// 1. Sets the initial state when the app loads
+
+//1. Sets the initial state when the app loads
 const initialState = {
     expenses: [
         { id: "Shirt", name: 'Shirt', quantity: 0, unitprice: 500 },
@@ -67,6 +68,7 @@ const initialState = {
     ],
     Location: '£'
 };
+
 // 2. Creates the context this is the thing our components import and use to get the state
 export const AppContext = createContext();
 // 3. Provider component - wraps the components we want to give access to the state
